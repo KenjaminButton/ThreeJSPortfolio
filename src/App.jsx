a9f9eefc8366f61e9faf238ac1d30a6d1e25f209
KenjaminButton <kennethpchang@gmail.com>
2023-11-12 22:08:29 -0800

    Navbar component setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={'Home'} />
+          <Route path="/about" element={'About'} />
+          <Route path="/projects" element={'Projects'} />
+          <Route path="/contact" element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
