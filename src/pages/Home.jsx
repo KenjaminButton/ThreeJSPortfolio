0444593b6186ef10349d7a08da16ce52d45499d8
KenjaminButton <kennethpchang@gmail.com>
2023-11-12 22:51:22 -0800

    Canvas installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Canvas} from '@react-three/fiber'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'  >
+        Popup Introducing Myself
+      </div> */}
+      <Canvas>
+        
+      </Canvas>
+
+    </section>
   )
 }
 
