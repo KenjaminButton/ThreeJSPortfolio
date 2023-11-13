5b5f40d72545968aaa2c6e9539e41a283c8d9f27
KenjaminButton <kennethpchang@gmail.com>
2023-11-12 22:00:54 -0800

    Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
+
