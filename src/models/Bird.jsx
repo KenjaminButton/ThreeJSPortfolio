5f481cf04586cf0b13584c578cb853f8101f34ae
KenjaminButton <kennethpchang@gmail.com>
2023-11-19 20:50:59 -0800

    Bird is flapping wings on full screen

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import {useRef, useEffect} from 'react';
 
 import birdScene from '../assets/3d/bird.glb';
-import { useGLTF } from '@react-three/drei';
+import { useAnimations, useGLTF } from '@react-three/drei';
 
 const Bird = () => {
+  const birdRef = useRef()
   const {scene, animations} = useGLTF(birdScene)
+  const {actions} = useAnimations(animations, birdRef)
+
+  useEffect( () => {
+    actions['Take 001'].play();
+  }, [])
 
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh 
+      position={[-5, 2, 1]} 
+      scale={[0.003, 0.003, 0.003]} 
+      ref={birdRef}
+    >
       <primitive object={scene} />
     </mesh>
   )
