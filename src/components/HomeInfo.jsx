ceea61bba210a74dbdfe906889e781de48924a2b
KenjaminButton <kennethpchang@gmail.com>
2023-11-21 09:56:10 -0800

    neo-brutalism-purple added to HomeInfo component.

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const renderContent = {
   1: (
-    <h1>1</h1>
+    <h1 className='sm: text-xl sm: leading-snug text-center neo-brutalism-purple py-4 px-8 text-white mx-5'>
+      Hi! I am <span className='font-semibold'>Kenneth</span> 😆
+      <br/>
+       My programming name is KenjaminButton!
+       <br/>
+       I'm a software engineer from San Jose, California
+    </h1>
   ),
   2: (
     <h1>2</h1>
@@ -13,6 +19,12 @@ const renderContent = {
   4: (
     <h1>4</h1>
   )
+}
+
+const InfoBox = ( {text, link, btnText} ) => {
+  <div className="info-box">
+    {text}
+  </div>
 }
 
 const HomeInfo = ({currentStage}) => {
