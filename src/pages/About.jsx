e1e1a0da2850837bb0d93a56921a7ba003090196
KenjaminButton <kennethpchang@gmail.com>
2023-11-22 13:02:38 -0800

    Scaffold for About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='purple-gradient_text font-semibold drop-shadow'>Kenneth</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>Software Engineer based in the Bay Area, California, specializing in full-stack web development.</p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
