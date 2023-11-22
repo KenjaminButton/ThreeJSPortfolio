08174a3ce77f87aae3a1b4f53821a727841a779d
KenjaminButton <kennethpchang@gmail.com>
2023-11-22 12:44:01 -0800

    Custom alert message/hook set up for Contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       {alert.show && <Alert {...alert} />}
-      <Alert {...alert} />
+      
 
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
