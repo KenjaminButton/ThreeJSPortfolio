2988efd51debda19201f9071780e1096343a7eaf
KenjaminButton <kennethpchang@gmail.com>
2023-11-23 10:17:52 -0800

    Skills Icons rendered. Very incomplete, but used as a placeholder for now

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
@@ -14,7 +15,17 @@ const About = () => {
         <h3 className='subhead-text'>My Skills</h3>
 
         <div className='mt-16 flex flex-wrap gap-12'>
-          
+          {skills.map( (skill) => (
+            <div>
+              <div>
+                <img 
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
